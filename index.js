568d8fb8e5e5e4031720af9c06e84033d86b37c8
Jay Milledge <jm@jay-media.de>
2022-05-27 15:14:40 +0200

    Add list and li => js

--- index.js
+++ index.js
@@ -4,13 +4,28 @@ fetch('https://pokeapi.co/api/v2/pokemon/')
 
 function data(pokemonData) {
   const pokemon = pokemonData.results;
+  const pokemonList = document.createElement('ul');
+  document.body.append(pokemonList);
+
   pokemon.forEach(eachPokemon => {
     fetch(eachPokemon.url)
       .then(response => response.json())
       .then(pokemonDetails);
 
     function pokemonDetails(pokemonDetailsEach) {
-      console.log(pokemonDetailsEach.name);
+      const pokemonAllData = pokemonDetailsEach;
+
+      const pokemonCard = document.createElement('li');
+      pokemonCard.className = 'pokemon-list';
+      pokemonList.append(pokemonCard);
     }
   });
 }
+
+/// name
+
+// height
+
+// weight
+
+//////////////  Content
