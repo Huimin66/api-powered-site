3f9c77b041aab14d2e80fda31045a19dd2cb8dc3
Jay Milledge <jm@jay-media.de>
2022-05-27 14:52:49 +0200

    Add each pokemon fetched

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hello world!');
+fetch('https://pokeapi.co/api/v2/pokemon/')
+  .then(response => response.json())
+  .then(data);
+
+function data(pokemonData) {
+  const pokemon = pokemonData.results;
+  pokemon.forEach(eachPokemon => {
+    fetch(eachPokemon.url)
+      .then(response => response.json())
+      .then(pokemonDetails);
+
+    function pokemonDetails(pokemonDetailsEach) {
+      console.log(pokemonDetailsEach.name);
+    }
+  });
+}
